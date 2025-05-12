eec78b92049f548ca93d5fdc8c90b726117836c4
Abhishek <abhiaskar.18@gmail.com>
2025-05-12 19:59:51 +0530

    Centralised error handling

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,20 +1,60 @@
-const productService = require('../service/productService');
 const path = require('path');
+const { sendErrorResponse, sendResponse } = require('../utils/response');
 
 const getProducts = (req, res) => {
-    res.sendFile(path.join(__dirname, "..", "view", "product.html"))
+    try {
+        res.sendFile(path.join(__dirname, "..", "view", "product.html"));
+    } catch (err) {
+        sendErrorResponse(res, {
+            message: 'Failed to load product page',
+            statusCode: 500
+        });
+    }
 };
 
 const getProductById = (req, res) => {
-    const id = req.params.id;
-    const result = productService.getProductById(id);
-    res.send(result);
+    try {
+        const id = req.params.id;
+
+        if (id > 10) {
+            return sendErrorResponse(res, {
+                message: 'Product not found',
+                statusCode: 404
+            });
+        }
+
+        const result = {
+            id: id,
+            name: "Sample Product"
+        };
+
+        return sendResponse(res, result, 200);
+    } catch (err) {
+        sendErrorResponse(res, {
+            message: 'Failed to get product by ID',
+            statusCode: 500
+        });
+    }
 };
 
 const postProduct = (req, res) => {
-    // const result = productService.addNewProduct();
-    const data = req.body;
-    res.json({value:data.productName});
+    try {
+        const data = req.body;
+
+        if (!data.productName) {
+            return sendErrorResponse(res, {
+                message: "productName is required",
+                statusCode: 400
+            });
+        }
+
+        return res.status(201).json({ value: data.productName });
+    } catch (err) {
+        sendErrorResponse(res, {
+            message: 'Failed to create product',
+            statusCode: 500
+        });
+    }
 };
 
 module.exports = {
